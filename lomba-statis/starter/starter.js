5ece88ff8bd47fd2981be4e55666227bd9c13620
rafie pradipta <rafie0917@gmail.com>
2022-08-28 15:59:53 +0700

    initial commit

---
+++ lomba-statis/starter/starter.js
@@ -0,0 +1,11 @@
+searchForm = document.querySelector('.search-form');
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
